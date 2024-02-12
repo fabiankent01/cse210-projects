e3f680e0f00a1ad9c69937a017ee16d507a760d5
fabiankent01 <fabiankent01@gmail.com>
2024-02-12 12:19:23 +0100

    new work

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 30));
+        shapes.Add(new Rectangle("Green", 40, 30));
+
+        foreach (Shape item in shapes)
+        {
+            double pay = item.GetArea();
+            string color = item.GetColor();
+            Console.WriteLine("Area: "+pay);
+            Console.WriteLine("Color: " + color);
+            Console.WriteLine();
+        }
     }
 }
---
+++ prepare/Learning05/Rectangle.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Rectangle : Shape
+{
+    private int _length;
+    private int _width;
+
+    public Rectangle(string color, int length, int width) : base (color)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
